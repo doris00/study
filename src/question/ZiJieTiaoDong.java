8629adf372ecf51cc36f782b1caae26a44045f60
doris00 <641896396@qq.com>
2021-09-27 17:20:55 +0800

    字节一面手撕

---
+++ src/question/ZiJieTiaoDong.java
@@ -0,0 +1,28 @@
+package question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 求字符串的不重复子串的最长长度
+ */
+public class ZiJieTiaoDong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Map<Character, Integer> temp = new HashMap<>();
+        int pre = 0;
+        int ans = 1;
+        for (int i = 0; i < s.length(); ++i) {
+            if (temp.containsKey(s.charAt(i))) {
+                pre = temp.get(s.charAt(i));
+            }
+            temp.put(s.charAt(i), i);
+            if((i - pre) > ans) {
+                ans = i - pre;
+            }
+        }
+        System.out.println(ans);
+    }
+}
